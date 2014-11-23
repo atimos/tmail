1c0a2e2b9a79a7d30f842eebb15ecb4f81390303
Alexander Seppälä <dev@timot.se>
2014-11-23 10:47:30 +0100

    moved to rust-http

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,139 @@
-extern crate iron;
+extern crate time;
 extern crate http;
 extern crate url;
+extern crate "conduit-mime-types" as mime;
+
 
 use std::os;
+use std::io::fs::PathExtensions;
+use std::io::net::ip::{SocketAddr, Ipv4Addr};
+use std::io::Writer;
 use std::io::File;
-use std::io::net::ip::Ipv4Addr;
-use http::headers;
+
 use url::Url;
-use iron::{Iron, Request, Response, IronResult, Set};
-use iron::response::modifiers::{Status, Body};
-use iron::status;
 
-fn file(request: &mut Request) -> IronResult<Response> {
-	let root_path = os::self_exe_path().unwrap().join("client");
+use mime::Types;
 
-	let url = Url::parse(request.url.to_string().as_slice()).ok();
+use http::server::request::{AbsoluteUri, AbsolutePath};
+use http::server::{Config, Server, Request, ResponseWriter};
+use http::headers::content_type::MediaType;
+use http::status::NotFound;
 
-	let content_path = url.and_then(|url| {
-		match url.path() {
-			Some(path_collection) => {
-				let mut path = root_path.clone();
+#[deriving(Clone)]
+struct TmailServer {
+	ip: std::io::net::ip::IpAddr,
+	port: u16,
+	root: Path,
+	mime: std::sync::Arc<Types>
+}
 
-				for part in path_collection.iter() {
-					path.push(part);
+impl TmailServer {
+	fn get_real_path(&self, host: Option<http::headers::host::Host>, uri: http::server::request::RequestUri) -> Option<Path> {
+		let url = match uri {
+			AbsolutePath(ref path) => {
+				let uri = host.and_then(|host| {
+					let mut uri = "http://".to_string();
+
+					uri.push_str(host.to_string().as_slice());
+					uri.push_str(path.as_slice());
+
+					Some(uri.as_slice().clone())
+				});
+
+				match uri {
+					Some(uri) => {
+						Url::parse(uri.as_slice()).ok()
+					},
+					_ => { None }
 				}
 
-				Some(path)
+			}
+			AbsoluteUri(uri) => {
+				Some(uri)
+			}
+			_ => { None }
+		};
+
+		url.and_then(|url| {
+			match url.path() {
+				Some(path_collection) => {
+					let mut path = self.root.clone();
+
+					for part in path_collection.iter() {
+						path.push(part);
+					}
+
+					if self.root.is_ancestor_of(&path) && path.is_file() {
+						Some(path)
+					} else {
+						None
+					}
+
+				},
+				_ => None
+			}
+		})
+	}
+
+	fn get_mime(&self, path: &Option<Path>) -> Option<MediaType> {
+		let mime_str = match path {
+			&Some(ref path) => {
+				self.mime.mime_for_path(path)
 			},
-			_ => None
-		}
-	}).and_then(|path| {
-		if root_path.is_ancestor_of(&path) {
-			Some(path)
-		} else {
-			None
-		}
-	}).and_then(|path| {
-		let content = File::open(&path).read_to_end();
-		match content {
-			Ok(content) => { Some((content, path)) }
-			Err(_) => None
-		}
-	});
+			&None => {
+				"text/plain"
+			}
+		};
 
-	match content_path {
-		Some((content, path)) => {
-			let mut response = Response::new().set(Status(status::Ok)).set(Body(content));
+		let mut mime_iter = mime_str.split('/');
 
-			let (type_, subtype) = match path.extension_str() {
-				Some("html") => ("text", "html"),
-				Some("css") => ("text", "css"),
-				Some("js") => ("application", "javascript"),
-				Some("json") => ("application", "json"),
-				_ => ("plain", "text")
-			};
-
-			response.headers.content_type = Some(headers::content_type::MediaType {
-				type_: String::from_str(type_),
-				subtype: String::from_str(subtype),
-				parameters: Vec::new()
-			});
-
-			Ok(response)
-		}
-		_ => { Ok(Response::new().set(Status(status::NotFound))) }
+		Some(MediaType {
+			type_: mime_iter.next().unwrap().to_string(),
+			subtype: mime_iter.next().unwrap().to_string(),
+			parameters: vec!(("charset".to_string(), "UTF-8".to_string()))
+		})
 	}
 }
 
+impl Server for TmailServer {
+	fn get_config(&self) -> Config {
+		Config { bind_address: SocketAddr { ip: self.ip, port: self.port } }
+	}
+
+	fn handle_request(&self, r: Request, w: &mut ResponseWriter) {
+		let path = self.get_real_path(r.headers.host.clone(), r.request_uri);
+
+		w.headers.content_type = self.get_mime(&path);
+
+		w.headers.date = Some(time::now_utc());
+
+		match path {
+			Some(path) => {
+					match File::open(&path).read_to_end() {
+						Ok(content) => {
+							w.write(content.as_slice()).unwrap();
+						}
+						Err(_) => {
+							w.status = NotFound;
+							w.write(b"Page not found").unwrap();
+						}
+					}
+			}
+			_ => {
+				w.status = NotFound;
+				w.write(b"Page not found").unwrap();
+			}
+		}
+	}
+}
+
+
 fn main() {
-	Iron::new(file).listen(Ipv4Addr(127, 0, 0, 1), 8080);
+	let server = TmailServer {
+		port: 3000,
+		ip: Ipv4Addr(127, 0, 0, 1),
+		root: os::self_exe_path().unwrap().join("client"),
+		mime: std::sync::Arc::new(Types::new().unwrap())
+	};
+	server.serve_forever();
 }
